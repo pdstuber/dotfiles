76ca98f64f401195fb25ecb92eaff3f306e68652
Philipp Stuber <philipp@stuber.io>
2023-11-25 10:24:17 +0100

    stuff

--- apply.sh
+++ apply.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env sh
-for FILE in ".p10k.zsh" ".zshrc" ".hyper.js"; do
-  ln -s "$(pwd)/${FILE}" "${HOME}/"
-done
+nix build .#darwinConfigurations.philipp-macbook.fritz.box.system
--- flake.nix
+++ flake.nix
@@ -1,23 +1,101 @@
 {
-  description = "Philipp's Home Manager flake";
+  description = "Philipp's darwin system";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-unstable = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+
+    nixpkgs-stable = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-23.05";
+    };
+
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
+    home-manager = {
+      type = "github";
+      owner = "nix-community";
+      repo = "home-manager";
+      ref = "release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
   };
 
-  outputs = inputs: {
-    defaultPackage.aarch64-darwin = home-manager.defaultPackage.aarch64-darwin;
- 
-    homeConfigurations = {
-	    # TODO: Modify "your.username" below to match your username
-      "pdstuber" = inputs.home-manager.lib.homeManagerConfiguration {
-        system = "aarch64-darwin"; # TODO: replace with x86_64-linux on Linux
-        homeDirectory = "/Users/pdstuber"; # TODO: make this match your home directory
-        username = "pdstuber"; # TODO: Change to your username
-        configuration.imports = [ ./home.nix ];
+  outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
+  let
+
+    inherit (darwin.lib) darwinSystem;
+    inherit (inputs.nixpkgs-unstable.lib) attrValues makeOverridable optionalAttrs singleton;
+
+    # Configuration for `nixpkgs`
+    nixpkgsConfig = {
+      config = { allowUnfree = true; };
+    }; 
+  in
+  {
+    darwinConfigurations = rec {
+      "FORM3-PHILIPPSTUBER" = darwinSystem {
+        system = "aarch64-darwin";
+        modules = attrValues self.darwinModules ++ [ 
+          # Main `nix-darwin` config
+          ./configuration.nix
+          # `home-manager` module
+          home-manager.darwinModules.home-manager
+          {
+            nixpkgs = nixpkgsConfig;
+            # `home-manager` config
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.pdstuber = import ./home.nix;            
+          }
+        ];
       };
+      "philipp-macbook" = darwinSystem {
+        system = "aarch64-darwin";
+        modules = attrValues self.darwinModules ++ [ 
+          # Main `nix-darwin` config
+          ./configuration.nix
+          # `home-manager` module
+          home-manager.darwinModules.home-manager
+          {
+            nixpkgs = nixpkgsConfig;
+            # `home-manager` config
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.pdstuber = import ./home.nix;            
+          }
+        ];
+      };
+    };
+
+    darwinModules = {
+      programs-nix-index = 
+        # Additional configuration for `nix-index` to enable `command-not-found` functionality with Fish.
+        { config, lib, pkgs, ... }:
+
+        {
+          config = lib.mkIf config.programs.nix-index.enable {
+            programs.fish.interactiveShellInit = ''
+              function __fish_command_not_found_handler --on-event="fish_command_not_found"
+                ${if config.programs.fish.useBabelfish then ''
+                command_not_found_handle $argv
+                '' else ''
+                ${pkgs.bashInteractive}/bin/bash -c \
+                  "source ${config.programs.nix-index.package}/etc/profile.d/command-not-found.sh; command_not_found_handle $argv"
+                ''}
+              end
+            '';
+            };
+        };
     };
   };
 }
